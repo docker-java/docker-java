1aa0242749aebf33409c6163aa87fd260099b2d4
kpelykh <kpelykh@gmail.com>
2013-07-19 12:29:33 -0700

    revert back stream parameter for logContainer

--- src/main/java/com/kpelykh/docker/client/DockerClient.java
+++ src/main/java/com/kpelykh/docker/client/DockerClient.java
@@ -442,7 +442,7 @@ public class DockerClient
         params.add("logs", "1");
         params.add("stdout", "1");
         params.add("stderr", "1");
-        params.add("stream", "1");
+        //params.add("stream", "1"); this parameter keeps stream open indindefinitely
 
         WebResource webResource = client.resource(restEndpointUrl + String.format("/containers/%s/attach", containerId))
                 .queryParams(params);
