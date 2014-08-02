c0330dc2d9d2223c4e997a18343bfc182fe402a6
Marcus Linke <linke@newsaktuell.de>
2014-08-02 12:41:34 +0200

    Fix issue #36

--- src/main/java/com/github/dockerjava/client/command/AttachContainerCmd.java
+++ src/main/java/com/github/dockerjava/client/command/AttachContainerCmd.java
@@ -124,7 +124,7 @@ public class AttachContainerCmd extends	AbstrDockerCmd<AttachContainerCmd, Clien
 		try {
 			LOGGER.trace("POST: {}", webResource);
 			return webResource.accept(MediaType.APPLICATION_OCTET_STREAM_TYPE)
-					.post(ClientResponse.class, params);
+					.post(ClientResponse.class);
 		} catch (UniformInterfaceException exception) {
 			if (exception.getResponse().getStatus() == 400) {
 				throw new DockerException("bad parameter");
