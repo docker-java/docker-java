1ad712e14e65bdba534c799467db0cda335916a2
Alex Collins <alex.e.c@gmail.com>
2014-11-08 23:26:30 +0000

    1

--- docker-auth-registry/nginx/build.sh
+++ docker-auth-registry/nginx/build.sh
@@ -3,8 +3,10 @@ set -eu
 
 
 echo "enter "registry" each time you are asked for a pass-phase"
-echo "enter "registry" when requested for a common name"
+echo "enter "nginx" when requested for a common name"
 echo "press enter for everything else"
+
+htpasswd -bc docker-registry.htpasswd registry registry
 
 echo 01 > ca.srl
 openssl genrsa -des3 -out ca-key.pem 2048
@@ -15,13 +17,3 @@ openssl req -subj "/CN=registry" -new -key server-key.pem -out server.csr
 openssl x509 -req -days 365 -in server.csr -CA ca.pem -CAkey ca-key.pem -out server-cert.pem
 
 openssl rsa -in server-key.pem -out server-key.pem
-
-if [ "$(which boot2docker)" != "" ]; then
-    B=$(echo $DOCKER_HOST|sed 's/.*\/\(.*\):.*/\1/')
-
-    scp -i ~/.ssh/id_boot2docker ca.pem docker@$B:
-
-    echo "mkdir /etc/docker/certs.d/localhost:5443/" | boot2docker ssh
-    echo "cat ca.pem > /etc/docker/certs.d/localhost:5443/ca.crt" | boot2docker ssh
-fi
-
