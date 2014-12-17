fc6429be2dd8b0248819bf3fa741dc2487b7bd5a
Harald Albers <github@albersweb.de>
2014-12-17 18:52:59 +0100

    Parser and toString for RestartPolicy

---
+++ src/test/java/com/github/dockerjava/api/model/RestartPolicy_toStringTest.java
@@ -0,0 +1,25 @@
+package com.github.dockerjava.api.model;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class RestartPolicy_toStringTest {
+
+	@DataProvider(name = "input")
+	public Object[][] restartPolicies() {
+		return new Object[][] { 
+				{ "no" },
+				{ "always" },
+				{ "on-failure" },
+				{ "on-failure:2" }
+				};
+	}
+
+	@Test(dataProvider = "input")
+	public void serializationWithoutCount(String policy) throws Exception {
+		assertEquals(RestartPolicy.parse(policy).toString(), policy);
+	}
+
+}
