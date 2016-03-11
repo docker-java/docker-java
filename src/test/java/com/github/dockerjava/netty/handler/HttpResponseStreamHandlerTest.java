3b3399d5674d68590de4da2107ec56a6688a57d0
Alexander Koshevoy <tejksat@users.noreply.github.com>
2016-03-11 15:01:51 +0300

    Fixes skipping 0xff bytes from result stream returned by Netty implementation of copy file/archive commands

---
+++ src/test/java/com/github/dockerjava/netty/handler/HttpResponseStreamHandlerTest.java
@@ -0,0 +1,51 @@
+package com.github.dockerjava.netty.handler;
+
+import com.github.dockerjava.core.async.ResultCallbackTemplate;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import org.apache.commons.io.IOUtils;
+import org.mockito.Mockito;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.InputStream;
+
+/**
+ * @author Alexander Koshevoy
+ */
+public class HttpResponseStreamHandlerTest {
+    @Test
+    public void testNoBytesSkipped() throws Exception {
+        ResultCallbackTest callback = new ResultCallbackTest();
+        HttpResponseStreamHandler streamHandler = new HttpResponseStreamHandler(callback);
+        ChannelHandlerContext ctx = Mockito.mock(ChannelHandlerContext.class);
+        ByteBuf buffer = generateByteBuf();
+        streamHandler.channelRead0(ctx, buffer);
+        streamHandler.channelReadComplete(ctx);
+
+        Assert.assertTrue(IOUtils.contentEquals(callback.getInputStream(), new ByteBufInputStream(buffer)));
+    }
+
+    private ByteBuf generateByteBuf() {
+        byte[] array = new byte[256];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (byte) i;
+        }
+        return Unpooled.copiedBuffer(array);
+    }
+
+    private static class ResultCallbackTest extends ResultCallbackTemplate<ResultCallbackTest, InputStream> {
+        private InputStream stream;
+
+        @Override
+        public void onNext(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public InputStream getInputStream() {
+            return stream;
+        }
+    }
+}
