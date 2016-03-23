8f3aa9c18e5f53427f5c15c0676d10b679d77f16
Gillouard Frédéric <frederic.gillouard@arkea.com>
2016-03-23 16:59:16 +0100

    Patch save image with tag

--- src/main/java/com/github/dockerjava/jaxrs/SaveImageCmdExec.java
+++ src/main/java/com/github/dockerjava/jaxrs/SaveImageCmdExec.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import com.github.dockerjava.api.command.SaveImageCmd;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.jaxrs.util.WrappedResponseInputStream;
+import com.google.common.base.Strings;
 
 public class SaveImageCmdExec extends AbstrSyncDockerCmdExec<SaveImageCmd, InputStream> implements SaveImageCmd.Exec {
     private static final Logger LOGGER = LoggerFactory.getLogger(SaveImageCmdExec.class);
@@ -22,8 +23,13 @@ public class SaveImageCmdExec extends AbstrSyncDockerCmdExec<SaveImageCmd, Input
 
     @Override
     protected InputStream execute(SaveImageCmd command) {
-        WebTarget webResource = getBaseResource().path("/images/" + command.getName() + "/get").queryParam("tag",
-                command.getTag());
+        // If tag is present, only tar the specific image
+        // else tar all the images with the same name
+        String name = command.getName();
+        if (!Strings.isNullOrEmpty(command.getTag())) {
+            name += ":" + command.getTag();
+        }
+        WebTarget webResource = getBaseResource().path("/images/" + name + "/get");
 
         LOGGER.trace("GET: {}", webResource);
         Response response = webResource.request().accept(MediaType.APPLICATION_JSON).get();
