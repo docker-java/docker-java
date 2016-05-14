381b7db28fd1007dea34b491c57fcd2ba8108591
Lappas Dionysis <dio@freelabs.net>
2016-05-15 02:07:25 +0300

    Fixed docker client name

--- src/test/java/com/github/dockerjava/core/command/CopyArchiveToContainerCmdImplTest.java
+++ src/test/java/com/github/dockerjava/core/command/CopyArchiveToContainerCmdImplTest.java
@@ -134,19 +134,19 @@ public class CopyArchiveToContainerCmdImplTest extends AbstractDockerClientTest 
         // create a test container which starts and waits 3 seconds for the
         // script to be copied to the container's home dir and then executes it
         String containerCmd = "sleep 3; /home/" + scriptPath.getFileName().toString();
-        CreateContainerResponse container = docker.createContainerCmd("busybox")
+        CreateContainerResponse container = dockerClient.createContainerCmd("busybox")
                 .withName("test")
                 .withCmd("/bin/sh", "-c", containerCmd)
                 .exec();
         // start the container
-        docker.startContainerCmd(container.getId()).exec();
+        dockerClient.startContainerCmd(container.getId()).exec();
         // copy script to container home dir
-        docker.copyArchiveToContainerCmd(container.getId())
+        dockerClient.copyArchiveToContainerCmd(container.getId())
                 .withRemotePath("/home")
                 .withHostResource(scriptPath.toString())
                 .exec();
         // await exid code
-        int exitCode = docker.waitContainerCmd(container.getId())
+        int exitCode = dockerClient.waitContainerCmd(container.getId())
                 .exec(new WaitContainerResultCallback())
                 .awaitStatusCode();
         // check result
