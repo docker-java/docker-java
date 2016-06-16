aa6eb70e04176434224eedbe20aaadc61202666a
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-06-17 02:13:44 +0300

    try install in other way

--- travis-before-install.sh
+++ travis-before-install.sh
@@ -8,12 +8,14 @@ set -exu
 sudo apt-get update
 sudo apt-get install -q -y wget
 
-mkdir "${HOME}/.cache" || :
-pushd "${HOME}/.cache"
- wget -N "https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_${DOCKER_VERSION}_amd64.deb"
- sudo apt-get -f install
- sudo dpkg -i "$(ls *${DOCKER_VERSION}*)"
-popd
+
+./get-docker-com.sh
+#mkdir "${HOME}/.cache" || :
+#pushd "${HOME}/.cache"
+# wget -N "https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_${DOCKER_VERSION}_amd64.deb"
+# sudo apt-get -f install
+# sudo dpkg -i "$(ls *${DOCKER_VERSION}*)"
+#popd
 
 echo 'DOCKER_OPTS="-H=unix:///var/run/docker.sock -H=tcp://127.0.0.1:2375"'
 sudo restart docker
