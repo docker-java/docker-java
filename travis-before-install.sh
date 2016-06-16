7af2441a67bd8e0b5b0a43295110e77d7745b173
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-06-17 01:53:35 +0300

    fix file loojup

--- travis-before-install.sh
+++ travis-before-install.sh
@@ -10,9 +10,9 @@ sudo apt-get install -q -y wget
 
 mkdir "${HOME}/.cache" || :
 pushd "${HOME}/.cache"
- wget "https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_${DOCKER_VERSION}_amd64.deb"
+ wget -N "https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_${DOCKER_VERSION}_amd64.deb"
  sudo apt-get -f install
- sudo dpkg -i "$(ls ${DOCKER_VERSION})"
+ sudo dpkg -i "$(ls *${DOCKER_VERSION}*)"
 popd
 
 echo 'DOCKER_OPTS="-H=unix:///var/run/docker.sock -H=tcp://127.0.0.1:2375"'
