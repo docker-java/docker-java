da5b7376d97493cbb4f016a08a3c4296987df93e
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-06-17 09:37:37 +0300

    not logging

--- travis-before-install.sh
+++ travis-before-install.sh
@@ -16,7 +16,8 @@ sudo apt-get install -q -y wget
 # sudo apt-get -f install
 # sudo dpkg -i "$(ls *${DOCKER_VERSION}*)"
 #popd
-rm -f "src/test/resources/logback.xml"
+#rm -f "src/test/resources/logback.xml"
+mv "src/test/resources/travis-logback.xml" "src/test/resources/logback.xml"
 
 echo 'DOCKER_OPTS="-H=unix:///var/run/docker.sock -H=tcp://127.0.0.1:2375"' | sudo tee -a /etc/default/docker
 sudo restart docker
