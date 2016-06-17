cb6d38714f578fc8635bb29b900441ada6400882
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-06-17 13:52:53 +0300

    rename var

--- travis-after-success.sh
+++ travis-after-success.sh
@@ -2,7 +2,7 @@
 
 
 codecov
-if [[ $TRAVIS_BRANCH == "master" ]] && [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $COMPILE_ONLY == "true" ]];
+if [[ $TRAVIS_BRANCH == "master" ]] && [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $MAIN_BUILD == "true" ]];
 then
     cat <<EOF >> ~/settings.xml
 <settings>
