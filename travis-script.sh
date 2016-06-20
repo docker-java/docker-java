a7afe55f80f636bb78609c84f147b9ffcfc05b87
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-06-20 03:46:12 +0300

    fix

--- travis-script.sh
+++ travis-script.sh
@@ -2,15 +2,18 @@
 
 set -ex
 
-if [ "${FAST_BUILD}" == true ]; then
-    if [ "$COVERITY" == true ]; then
+IS_COVERITY_SCAN_BRANCH=`ruby -e "puts '${TRAVIS_BRANCH}' =~ /\\A$COVERITY_SCAN_BRANCH_PATTERN\\z/ ? 1 : 0"`
+
+
+if [ "${FAST_BUILD}" == "true" ]; then
+    if [ "$COVERITY" == "true" ] && [ "$IS_COVERITY_SCAN_BRANCH" = "1" ]; then
         export COVERITY_SCAN_BUILD_COMMAND="mvn package"
         curl -s "https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh" | bash
     else
         mvn package
     fi
 else
-    if [ "$COVERITY" == true ] && [ "${TRAVIS_PULL_REQUEST}" != "true" ]; then
+    if [ "$COVERITY" == "true" ] && [ "$IS_COVERITY_SCAN_BRANCH" = "1" ]; then
         COVERITY_SCAN_BUILD_COMMAND="mvn verify"
         curl -s "https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh" | bash
     else
