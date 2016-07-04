199e27db662da73fe51c2af8f66ad595bd91b1cc
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-07-04 23:32:13 +0300

    Move scripts to dir. Fix coverity skip.

--- .travis/travis-script.sh
+++ .travis/travis-script.sh
@@ -8,14 +8,16 @@ IS_COVERITY_SCAN_BRANCH=`ruby -e "puts '${TRAVIS_BRANCH}' =~ /\\A$COVERITY_SCAN_
 if [ "${FAST_BUILD}" == "true" ]; then
     if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$COVERITY" == "true" ] && [ "$IS_COVERITY_SCAN_BRANCH" = "1" ]; then
         export COVERITY_SCAN_BUILD_COMMAND="mvn package"
-        curl -s "https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh" | bash
+        #curl -s "https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh" | bash
+        ./.travis/travisci_build_coverity_scan.sh
     else
         mvn package
     fi
 else
     if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$COVERITY" == "true" ] && [ "$IS_COVERITY_SCAN_BRANCH" = "1" ]; then
         COVERITY_SCAN_BUILD_COMMAND="mvn verify"
-        curl -s "https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh" | bash
+        #curl -s "https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh" | bash
+        ./.travis/travisci_build_coverity_scan.sh
     else
         mvn verify
     fi
