e8b8519bbfb32cc1c5f3b6533b8b32fe7950103a
tjlee <v.chernov@crystals.ru>
2016-07-29 17:11:02 +0300

    [tests] Added integration tests for pause / unpause commands (#647)
    * [tests] Added integration tests for pause / unpause commands
    * [tests] added missing netty tests; fixed javadoc text; fixed annotations;

---
+++ src/test/java/com/github/dockerjava/core/command/PauseCmdImplTest.java
@@ -0,0 +1,103 @@
+package com.github.dockerjava.core.command;
+
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.exception.InternalServerErrorException;
+import com.github.dockerjava.api.exception.NotFoundException;
+import com.github.dockerjava.client.AbstractDockerClientTest;
+import com.github.dockerjava.utils.ContainerUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.not;
+
+@Test(groups = {"integration", "ignoreInCircleCi"})
+public class PauseCmdImplTest extends AbstractDockerClientTest {
+
+    public static final Logger LOG = LoggerFactory.getLogger(PauseCmdImplTest.class);
+
+    @BeforeTest
+    public void beforeTest() throws Exception {
+        super.beforeTest();
+    }
+
+    @AfterTest
+    public void afterTest() {
+        super.afterTest();
+    }
+
+    @BeforeMethod
+    public void beforeMethod(Method method) {
+        super.beforeMethod(method);
+    }
+
+    @AfterMethod
+    public void afterMethod(ITestResult result) {
+        super.afterMethod(result);
+    }
+
+    @Test
+    public void pauseRunningContainer() {
+
+        CreateContainerResponse container = dockerClient.createContainerCmd("busybox").withCmd("sleep", "9999").exec();
+        LOG.info("Created container: {}", container.toString());
+        assertThat(container.getId(), not(isEmptyString()));
+
+        ContainerUtils.startContainer(dockerClient, container);
+
+        ContainerUtils.pauseContainer(dockerClient, container);
+    }
+
+    @Test(expectedExceptions = NotFoundException.class)
+    public void pauseNonExistingContainer() {
+
+        dockerClient.pauseContainerCmd("non-existing").exec();
+    }
+
+    @Test(expectedExceptions = InternalServerErrorException.class)
+    public void pauseStoppedContainer() {
+
+        CreateContainerResponse container = dockerClient.createContainerCmd("busybox").withCmd("sleep", "9999").exec();
+        LOG.info("Created container: {}", container.toString());
+        assertThat(container.getId(), not(isEmptyString()));
+
+        ContainerUtils.startContainer(dockerClient, container);
+
+        ContainerUtils.stopContainer(dockerClient, container);
+
+        dockerClient.pauseContainerCmd(container.getId()).exec();
+    }
+
+    @Test(expectedExceptions = InternalServerErrorException.class)
+    public void pausePausedContainer() {
+
+        CreateContainerResponse container = dockerClient.createContainerCmd("busybox").withCmd("sleep", "9999").exec();
+        LOG.info("Created container: {}", container.toString());
+        assertThat(container.getId(), not(isEmptyString()));
+
+        ContainerUtils.startContainer(dockerClient, container);
+
+        ContainerUtils.pauseContainer(dockerClient, container);
+
+        dockerClient.pauseContainerCmd(container.getId()).exec();
+    }
+
+    @Test(expectedExceptions = InternalServerErrorException.class)
+    public void pauseCreatedContainer() {
+
+        CreateContainerResponse container = dockerClient.createContainerCmd("busybox").withCmd("sleep", "9999").exec();
+        LOG.info("Created container: {}", container.toString());
+        assertThat(container.getId(), not(isEmptyString()));
+
+        dockerClient.pauseContainerCmd(container.getId()).exec();
+    }
+}
---
+++ src/test/java/com/github/dockerjava/utils/ContainerUtils.java
@@ -0,0 +1,70 @@
+package com.github.dockerjava.utils;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.command.InspectContainerResponse;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Container cmd utils
+ */
+public class ContainerUtils {
+
+    private ContainerUtils() {
+    }
+
+    /**
+     * Starts container and ensures that it running
+     *
+     * @param dockerClient docker client
+     * @param container    container
+     */
+    public static void startContainer(DockerClient dockerClient, CreateContainerResponse container) {
+        dockerClient.startContainerCmd(container.getId()).exec();
+
+        InspectContainerResponse inspectContainerResponse = dockerClient.inspectContainerCmd(container.getId()).exec();
+        assertThat(inspectContainerResponse.getState().getRunning(), is(true));
+    }
+
+    /**
+     * Pauses container and ensures that it paused
+     *
+     * @param dockerClient docker client
+     * @param container    container
+     */
+    public static void pauseContainer(DockerClient dockerClient, CreateContainerResponse container) {
+        dockerClient.pauseContainerCmd(container.getId()).exec();
+
+        InspectContainerResponse inspectContainerResponse = dockerClient.inspectContainerCmd(container.getId()).exec();
+        assertThat(inspectContainerResponse.getState().getPaused(), is(true));
+    }
+
+    /**
+     * Stops container and ensures that it stopped
+     *
+     * @param dockerClient docker client
+     * @param container    container
+     */
+    public static void stopContainer(DockerClient dockerClient, CreateContainerResponse container) {
+        dockerClient.stopContainerCmd(container.getId()).exec();
+
+        InspectContainerResponse inspectContainerResponse = dockerClient.inspectContainerCmd(container.getId()).exec();
+        assertThat(inspectContainerResponse.getState().getRunning(), is(false));
+    }
+
+    /**
+     * Unpauses container and ensures that it unpaused (running)
+     *
+     * @param dockerClient docker client
+     * @param container    container
+     */
+    public static void unpaseContainer(DockerClient dockerClient, CreateContainerResponse container) {
+        dockerClient.unpauseContainerCmd(container.getId()).exec();
+
+        InspectContainerResponse inspectContainerResponse = dockerClient.inspectContainerCmd(container.getId()).exec();
+        assertThat(inspectContainerResponse.getState().getPaused(), is(false));
+        assertThat(inspectContainerResponse.getState().getRunning(), is(true));
+    }
+}
