1870e0fc36d043295d77cb7ee241a81fd1e19844
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-08-17 00:53:30 +0300

    Sync event model to 1.24
    Signed-off-by: Kanstantsin Shautsou <kanstantsin.sha@gmail.com>

---
+++ src/main/java/com/github/dockerjava/api/model/EventType.java
@@ -0,0 +1,55 @@
+package com.github.dockerjava.api.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @since 1.24
+ */
+public enum EventType {
+    /**
+     * @since 1.24
+     */
+    CONTAINER("container"),
+
+    /**
+     * @since 1.24
+     */
+    DAEMON("daemon"),
+
+    /**
+     * @since 1.24
+     */
+    IMAGE("image"),
+    NETWORK("network"),
+    PLUGIN("plugin"),
+    VOLUME("volume");
+
+    private static final Map<String, EventType> EVENT_TYPES = new HashMap<>();
+
+    static {
+        for (EventType t : values()) {
+            EVENT_TYPES.put(t.name().toLowerCase(), t);
+        }
+    }
+
+    private String value;
+
+    EventType(@Nonnull String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static EventType forValue(String s) {
+        return EVENT_TYPES.get(s);
+    }
+}
