d0d842f87f2cf14953c46de7dccae7641690821c
Martin Root <mail@martin-root.de>
2016-08-17 14:56:44 +0200

    Add node to model package
    Add ListNodes implementation
    
    Fix error in ListNodesCmdExec
    
    Add FilterBuilder for swarmNodes, renamed Node to SwarmNode
    
    Update model classes for SwarmNodes
    
    add implementation for update,list,remove and inspect of a swarmNode
    
    Update model classes for docker-swarm-node
    
    Add methods for swarmNode to execFactories

---
+++ src/main/java/com/github/dockerjava/core/command/ListSwarmNodesCmdImpl.java
@@ -0,0 +1,57 @@
+package com.github.dockerjava.core.command;
+
+
+import com.github.dockerjava.api.command.ListSwarmNodesCmd;
+import com.github.dockerjava.api.model.SwarmNode;
+import com.github.dockerjava.core.util.SwarmNodesFiltersBuilder;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * List SwarmNodes
+ */
+public class ListSwarmNodesCmdImpl extends AbstrDockerCmd<ListSwarmNodesCmd, List<SwarmNode>> implements
+        ListSwarmNodesCmd {
+
+    private SwarmNodesFiltersBuilder filters = new SwarmNodesFiltersBuilder();
+
+    public ListSwarmNodesCmdImpl(Exec exec) {
+        super(exec);
+    }
+
+    @Override
+    public Map<String, List<String>> getFilters() {
+        return filters.build();
+    }
+
+    @Override
+    public ListSwarmNodesCmd withIdFilter(List<String> ids) {
+        checkNotNull(ids, "ids was not specified");
+        this.filters.withIds(ids);
+        return this;
+    }
+
+    @Override
+    public ListSwarmNodesCmd withNameFilter(List<String> names) {
+        checkNotNull(names, "names was not specified");
+        this.filters.withNames(names);
+        return this;
+    }
+
+    @Override
+    public ListSwarmNodesCmd withMembershipFilter(List<String> memberships) {
+        checkNotNull(memberships, "memberships was not specified");
+        this.filters.withNames(memberships);
+        return this;
+    }
+
+    @Override
+    public ListSwarmNodesCmd withRoleFilter(List<String> roles) {
+        checkNotNull(roles, "roles was not specified");
+        this.filters.withNames(roles);
+        return this;
+    }
+}
