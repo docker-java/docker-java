bc9d8b43eb60ecc8d3055fc5dd71515931804aff
Jonas Groß <gross.jonas@gmail.com>
2016-10-10 07:43:01 +0200

    fix wrong parameter name & add test - fix/avoid yet another star import

--- src/test/java/com/github/dockerjava/core/command/CommitCmdImplTest.java
+++ src/test/java/com/github/dockerjava/core/command/CommitCmdImplTest.java
@@ -1,11 +1,10 @@
 package com.github.dockerjava.core.command;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.testinfected.hamcrest.jpa.HasFieldWithValue.hasField;
-
-import java.lang.reflect.Method;
-
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.command.InspectImageResponse;
+import com.github.dockerjava.api.exception.DockerException;
+import com.github.dockerjava.api.exception.NotFoundException;
+import com.github.dockerjava.client.AbstractDockerClientTest;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -13,11 +12,15 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.github.dockerjava.api.exception.DockerException;
-import com.github.dockerjava.api.exception.NotFoundException;
-import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.command.InspectImageResponse;
-import com.github.dockerjava.client.AbstractDockerClientTest;
+import java.lang.reflect.Method;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.startsWith;
+import static org.testinfected.hamcrest.jpa.HasFieldWithValue.hasField;
 
 @Test(groups = "integration")
 public class CommitCmdImplTest extends AbstractDockerClientTest {
