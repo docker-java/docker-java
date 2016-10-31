616c3ad1cf8e68920c3259b7dcdf943903619cff
fb <fb@baqend.com>
2016-10-31 12:36:10 +0100

    add state and mounts fields to list container response introduced with API version 1.23

--- src/main/java/com/github/dockerjava/api/model/Container.java
+++ src/main/java/com/github/dockerjava/api/model/Container.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.dockerjava.api.command.InspectContainerResponse;
 import com.github.dockerjava.api.command.ListContainersCmd;
 import com.github.dockerjava.core.RemoteApiVersion;
 import org.apache.commons.lang.builder.EqualsBuilder;
@@ -51,6 +52,13 @@ public class Container implements Serializable {
     @JsonProperty("Labels")
     public Map<String, String> labels;
 
+    /**
+     * The containers status state can be one of created, restarting, running, paused, exited or dead
+     * @since ~{@link RemoteApiVersion#VERSION_1_23}
+     */
+    @JsonProperty("State")
+    private String state;
+
     @JsonProperty("Status")
     private String status;
 
@@ -73,6 +81,12 @@ public class Container implements Serializable {
      */
     @JsonProperty("HostConfig")
     private ContainerHostConfig hostConfig;
+
+    /**
+     * @since ~{@link RemoteApiVersion#VERSION_1_23}
+     */
+    @JsonProperty("Mounts")
+    private InspectContainerResponse.Mount mounts;
 
     /**
      * Docker API docs says "list of networks", but json names `networkSettings`.
@@ -102,6 +116,10 @@ public class Container implements Serializable {
 
     public Long getCreated() {
         return created;
+    }
+
+    public String getState() {
+        return state;
     }
 
     public String getStatus() {
@@ -150,6 +168,10 @@ public class Container implements Serializable {
     @CheckForNull
     public ContainerHostConfig getHostConfig() {
         return hostConfig;
+    }
+
+    public InspectContainerResponse.Mount getMounts() {
+        return mounts;
     }
 
     @Override
--- src/test/java/com/github/dockerjava/core/command/ListContainersCmdImplTest.java
+++ src/test/java/com/github/dockerjava/core/command/ListContainersCmdImplTest.java
@@ -1,5 +1,22 @@
 package com.github.dockerjava.core.command;
 
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.command.InspectContainerResponse;
+import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.client.AbstractDockerClientTest;
+import com.google.common.collect.ImmutableMap;
+import org.hamcrest.Matcher;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+
 import static ch.lambdaj.Lambda.filter;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -10,24 +27,6 @@ import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.startsWith;
 import static org.testinfected.hamcrest.jpa.HasFieldWithValue.hasField;
-
-import java.lang.reflect.Method;
-import java.util.List;
-import java.util.Map;
-
-import org.hamcrest.Matcher;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
-import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.command.InspectContainerResponse;
-import com.github.dockerjava.api.model.Container;
-import com.github.dockerjava.client.AbstractDockerClientTest;
-import com.google.common.collect.ImmutableMap;
 
 @Test(groups = "integration")
 public class ListContainersCmdImplTest extends AbstractDockerClientTest {
@@ -98,6 +97,7 @@ public class ListContainersCmdImplTest extends AbstractDockerClientTest {
         Container container2 = filteredContainers.get(0);
         assertThat(container2.getCommand(), not(isEmptyString()));
         assertThat(container2.getImage(), startsWith(testImage));
+        assertThat(container2.getState(), equalTo("running"));
     }
 
     @Test
