c49f598fb0ee79a4a3b463312da4aab1b55d3050
Sukhanov Stas <stas.sukhanov@gmail.com>
2017-01-25 22:33:49 +0500

    Add support for multiple certificates in ca.pem file
    A single PEM file can contain a number of certificates. Usually keys and certificates are split into different files, but ca.pem still can contain several certificates in some cases.

--- src/test/java/com/github/dockerjava/core/util/CertificateUtilsTest.java
+++ src/test/java/com/github/dockerjava/core/util/CertificateUtilsTest.java
@@ -1,5 +1,14 @@
 package com.github.dockerjava.core.util;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,6 +17,16 @@ import static org.hamcrest.core.Is.is;
 public class CertificateUtilsTest {
     private static final String baseDir = CertificateUtilsTest.class.getResource(
             CertificateUtilsTest.class.getSimpleName() + "/").getFile();
+
+    @BeforeClass
+    public static void init() {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME);
+    }
 
     @Test
     public void allFilesExist() {
@@ -47,5 +66,26 @@ public class CertificateUtilsTest {
     @Test
     public void keyMissing() {
         assertThat(CertificateUtils.verifyCertificatesExist(baseDir + "keyMissing"), is(false));
+    }
+
+    @Test
+    public void readCaCert() throws Exception {
+        String capem = readFileAsString("caTest/single_ca.pem");
+        KeyStore keyStore = CertificateUtils.createTrustStore(capem);
+        assertThat(keyStore.size(), is(1));
+        assertThat(keyStore.isCertificateEntry("ca-1"), is(true));
+    }
+
+    @Test
+    public void readMultipleCaCerts() throws Exception {
+        String capem = readFileAsString("caTest/multiple_ca.pem");
+        KeyStore keyStore = CertificateUtils.createTrustStore(capem);
+        assertThat(keyStore.size(), is(2));
+        assertThat(keyStore.isCertificateEntry("ca-1"), is(true));
+        assertThat(keyStore.isCertificateEntry("ca-2"), is(true));
+    }
+
+    private String readFileAsString(String path) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(baseDir + path)));
     }
 }
