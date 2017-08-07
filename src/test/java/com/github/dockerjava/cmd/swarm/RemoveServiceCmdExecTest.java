53fc789b0adf5d1b34190b6b6891a2e036f78ae0
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2017-08-08 02:42:38 +0300

    prepare 3.0.x branch

---
+++ src/test/java/com/github/dockerjava/cmd/swarm/RemoveServiceCmdExecTest.java
@@ -0,0 +1,54 @@
+package com.github.dockerjava.cmd.swarm;
+
+import com.github.dockerjava.api.exception.DockerException;
+import com.github.dockerjava.api.model.ContainerSpec;
+import com.github.dockerjava.api.model.Service;
+import com.github.dockerjava.api.model.ServiceSpec;
+import com.github.dockerjava.api.model.SwarmSpec;
+import com.github.dockerjava.api.model.TaskSpec;
+import com.google.common.collect.Lists;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+public class RemoveServiceCmdExecTest extends SwarmCmdTest {
+
+    public static final Logger LOG = LoggerFactory.getLogger(RemoveServiceCmdExecTest.class);
+    private static final String SERVICE_NAME = "theservice";
+
+    @Test
+    public void removeService() throws DockerException {
+        dockerRule.getClient().initializeSwarmCmd(new SwarmSpec()).withListenAddr("127.0.0.1").exec();
+
+        ServiceSpec spec = new ServiceSpec()
+                .withName(SERVICE_NAME)
+                .withTaskTemplate(new TaskSpec()
+                        .withContainerSpec(new ContainerSpec()
+                                .withImage("busybox")));
+
+        dockerRule.getClient().createServiceCmd(spec).exec();
+
+        spec.getTaskTemplate().getContainerSpec().withImage("ubuntu");
+
+        List<Service> services = dockerRule.getClient().listServicesCmd()
+                .withNameFilter(Lists.newArrayList(SERVICE_NAME))
+                .exec();
+
+        assertThat(services, hasSize(1));
+
+        dockerRule.getClient().removeServiceCmd(services.get(0).getId())
+                .exec();
+
+        services = dockerRule.getClient().listServicesCmd()
+                .withNameFilter(Lists.newArrayList(SERVICE_NAME))
+                .exec();
+
+        assertThat(services, hasSize(0));
+    }
+
+}
