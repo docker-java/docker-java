d45f0ff74dd6679985b0fcce6f54a67c24115d47
Albert Murillo Aguirre <amurillo@redpoints.com>
2017-08-14 15:19:13 +0200

    GITHUB Add environment variables to ExecCreateCmd (as stated on API v1.23)

--- src/test/java/com/github/dockerjava/core/command/ExecCreateCmdImplTest.java
+++ src/test/java/com/github/dockerjava/core/command/ExecCreateCmdImplTest.java
@@ -1,12 +1,20 @@
 package com.github.dockerjava.core.command;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.isEmptyString;
 import static org.hamcrest.Matchers.not;
 
+import java.io.ByteArrayOutputStream;
 import java.lang.reflect.Method;
 import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
+import com.github.dockerjava.api.async.ResultCallback;
+import com.github.dockerjava.api.command.ExecCreateCmd;
+import com.github.dockerjava.api.command.InspectContainerResponse;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -57,5 +65,37 @@ public class ExecCreateCmdImplTest extends AbstractDockerClientTest {
                 .withCmd("touch", "file.log").exec();
 
         assertThat(execCreateCmdResponse.getId(), not(isEmptyString()));
+    }
+
+    @Test
+    public void execCreateTestWithEnv() throws Exception {
+        String containerName = "generated_" + new SecureRandom().nextInt();
+        final String testVariable = "VARIABLE=success";
+
+        CreateContainerResponse container = dockerClient.createContainerCmd("busybox").withUser("root").withCmd("top")
+                .withName(containerName).exec();
+
+        LOG.info("Created container {}", container.toString());
+
+        assertThat(container.getId(), not(isEmptyString()));
+
+        dockerClient.startContainerCmd(container.getId()).exec();
+
+        ExecCreateCmdResponse execCreateCmdResponse = dockerClient.execCreateCmd(container.getId())
+                .withEnv(testVariable)
+                .withCmd("/bin/sh", "-c", "\"env | grep BASE_DIR\"").exec();
+
+        assertThat(execCreateCmdResponse.getId(), not(isEmptyString()));
+
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+
+        dockerClient.execStartCmd(execCreateCmdResponse.getId())
+                .withDetach(false)
+                .exec(new ExecStartResultCallback(stdout, System.err))
+                .awaitCompletion();
+
+        stdout.close();
+
+        assertEquals(stdout.toString(), testVariable);
     }
 }
