33b32e696ca463893c6d22c9956678133f0d1fd0
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2017-09-17 04:12:57 +0300

    fix image

--- src/test/java/com/github/dockerjava/cmd/CommitCmdTest.java
+++ src/test/java/com/github/dockerjava/cmd/CommitCmdTest.java
@@ -4,6 +4,7 @@ import com.github.dockerjava.api.command.CreateContainerResponse;
 import com.github.dockerjava.api.command.InspectImageResponse;
 import com.github.dockerjava.api.exception.DockerException;
 import com.github.dockerjava.api.exception.NotFoundException;
+import com.github.dockerjava.junit.DockerRule;
 import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -15,7 +16,10 @@ import static com.github.dockerjava.junit.DockerAssume.assumeNotSwarm;
 import static com.github.dockerjava.junit.DockerRule.DEFAULT_IMAGE;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.iterableWithSize;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.startsWith;
 import static org.testinfected.hamcrest.jpa.HasFieldWithValue.hasField;
@@ -43,7 +47,7 @@ public class CommitCmdTest extends CmdTest {
         LOG.info("Image Inspect: {}", inspectImageResponse.toString());
 
         assertThat(inspectImageResponse, hasField("container", startsWith(container.getId())));
-        assertThat(inspectImageResponse.getContainerConfig().getImage(), equalTo("busybox"));
+        assertThat(inspectImageResponse.getContainerConfig().getImage(), equalTo(DEFAULT_IMAGE));
 
         InspectImageResponse busyboxImg = dockerRule.getClient().inspectImageCmd("busybox").exec();
 
@@ -53,7 +57,9 @@ public class CommitCmdTest extends CmdTest {
     @Test
     public void commitWithLabels() throws DockerException {
 
-        CreateContainerResponse container = dockerRule.getClient().createContainerCmd("busybox").withCmd("touch", "/test").exec();
+        CreateContainerResponse container = dockerRule.getClient().createContainerCmd("busybox")
+                .withCmd("touch", "/test")
+                .exec();
 
         LOG.info("Created container: {}", container.toString());
         assertThat(container.getId(), not(isEmptyString()));
@@ -66,6 +72,7 @@ public class CommitCmdTest extends CmdTest {
         InspectImageResponse inspectImageResponse = dockerRule.getClient().inspectImageCmd(imageId).exec();
         LOG.info("Image Inspect: {}", inspectImageResponse.toString());
         Map<String, String> responseLabels = inspectImageResponse.getContainerConfig().getLabels();
+        assertThat(responseLabels.size(), is(2));
         assertThat(responseLabels.get("label1"), equalTo("abc"));
         assertThat(responseLabels.get("label2"), equalTo("123"));
     }
