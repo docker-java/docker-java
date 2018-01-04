a0f17dbb404ffe43cee0669e6cbfc35d8f264c94
Yuting Liu <yuting@sumologic.com>
2018-01-04 14:06:04 -0800

    Override object equals method in IoRecursiveConfig

--- src/main/java/com/github/dockerjava/api/model/IoRecursiveConfig.java
+++ src/main/java/com/github/dockerjava/api/model/IoRecursiveConfig.java
@@ -2,6 +2,9 @@ package com.github.dockerjava.api.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
 
 import javax.annotation.CheckForNull;
 import java.io.Serializable;
@@ -82,5 +85,20 @@ public class IoRecursiveConfig implements Serializable {
     public IoRecursiveConfig withValue(Long value) {
         this.value = value;
         return this;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
     }
 }
