2c2e301b4fae304f495730fd00fb9c61d6c92393
Dmitry Tretyakov <dtretyakov@gmail.com>
2018-09-14 18:50:49 +0200

    Add withFilter methods in ListNetworksCmd & ListVolumesCmd

--- src/main/java/com/github/dockerjava/api/command/ListNetworksCmd.java
+++ src/main/java/com/github/dockerjava/api/command/ListNetworksCmd.java
@@ -5,6 +5,7 @@ import com.github.dockerjava.core.RemoteApiVersion;
 
 import javax.annotation.CheckForNull;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +22,13 @@ public interface ListNetworksCmd extends SyncDockerCmd<List<Network>> {
     ListNetworksCmd withNameFilter(String... networkName);
 
     ListNetworksCmd withIdFilter(String... networkId);
+
+    /**
+     * @param filterName
+     * @param filterValues
+     *            - Show only networks where the filter matches the given values
+     */
+    ListNetworksCmd withFilter(String filterName, Collection<String> filterValues);
 
     interface Exec extends DockerCmdSyncExec<ListNetworksCmd, List<Network>> {
     }
--- src/main/java/com/github/dockerjava/api/command/ListVolumesCmd.java
+++ src/main/java/com/github/dockerjava/api/command/ListVolumesCmd.java
@@ -1,5 +1,6 @@
 package com.github.dockerjava.api.command;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -20,6 +21,13 @@ public interface ListVolumesCmd extends SyncDockerCmd<ListVolumesResponse> {
      *            - Show dangling volumes filter
      */
     ListVolumesCmd withDanglingFilter(Boolean dangling);
+
+    /**
+     * @param filterName
+     * @param filterValues
+     *            - Show only volumes where the filter matches the given values
+     */
+    ListVolumesCmd withFilter(String filterName, Collection<String> filterValues);
 
     interface Exec extends DockerCmdSyncExec<ListVolumesCmd, ListVolumesResponse> {
     }
