de1ff3ac8611580256154fbbc3b0f32e3bfbddce
Arik Sher <arik.sher@nex.com>
2019-01-21 18:51:24 +0200

    add equals hashCode and toString so comparison will work

--- src/main/java/com/github/dockerjava/api/model/GenericResource.java
+++ src/main/java/com/github/dockerjava/api/model/GenericResource.java
@@ -1,6 +1,10 @@
 package com.github.dockerjava.api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 
 import java.io.Serializable;
 
@@ -28,5 +32,20 @@ public abstract class GenericResource<T> implements Serializable {
     public GenericResource withValue(T value) {
         this.value = value;
         return this;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
     }
 }
