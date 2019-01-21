37fe232044cf4d42587d21e031006ddb3a80987f
Arik Sher <arik.sher@nex.com>
2019-01-21 19:09:43 +0200

    comment

--- src/main/java/com/github/dockerjava/api/model/DiscreteResourceSpec.java
+++ src/main/java/com/github/dockerjava/api/model/DiscreteResourceSpec.java
@@ -2,6 +2,11 @@ package com.github.dockerjava.api.model;
 
 import java.io.Serializable;
 
+/**
+ *
+ * since 1.24 generic type is String
+ * since 1.39 generic type is Integer
+ */
 public class DiscreteResourceSpec extends GenericResource<Integer> implements Serializable {
     private static final long serialVersionUID = 1L;
 }
