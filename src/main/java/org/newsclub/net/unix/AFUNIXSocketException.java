85382c3ed9d9ec5ee756e16e6b5f7d787fa89973
engadmin <engadmin@gmail.com>
2019-02-05 12:44:19 +0530

    Adding whitespaces to appease the maven checkstyle plugin

--- src/main/java/org/newsclub/net/unix/AFUNIXSocketException.java
+++ src/main/java/org/newsclub/net/unix/AFUNIXSocketException.java
@@ -2,16 +2,16 @@ package org.newsclub.net.unix;
 
 import java.net.SocketException;
 
-public class AFUNIXSocketException extends SocketException{
+public class AFUNIXSocketException extends SocketException {
     private static final long serialVersionUID = 1L;
     private final String socketFile;
 
     public AFUNIXSocketException(String reason) {
-        this(reason, (String)null);
+        this(reason, (String) null);
     }
 
     public AFUNIXSocketException(String reason, Throwable cause) {
-        this(reason, (String)null);
+        this(reason, (String) null);
         this.initCause(cause);
     }
 
