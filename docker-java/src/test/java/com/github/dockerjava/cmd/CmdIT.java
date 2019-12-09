69160f9826faace6de31c87133774d9b911a880e
Sergei Egorov <segorov@pivotal.io>
2019-12-09 11:16:23 +0100

    Test all transports (again :D)

--- docker-java/src/test/java/com/github/dockerjava/cmd/CmdIT.java
+++ docker-java/src/test/java/com/github/dockerjava/cmd/CmdIT.java
@@ -60,8 +60,7 @@ public abstract class CmdIT {
 
     @Parameterized.Parameters(name = "{index}:{0}")
     public static Iterable<FactoryType> data() {
-        // return Arrays.asList(FactoryType.values());
-        return Arrays.asList(FactoryType.OKHTTP);
+        return Arrays.asList(FactoryType.values());
     }
 
     @Parameterized.Parameter
