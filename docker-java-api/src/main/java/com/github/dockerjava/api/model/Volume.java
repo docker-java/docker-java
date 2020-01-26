0e4b1a76333ca20ec750240b53c130b263da5924
Sergei Egorov <bsideup@gmail.com>
2020-01-26 21:32:43 +0100

    Add japicmp to the API module and fix backward compatibility issues (#1310)
    * Add automated japicmp to the API module
    
    * restore backward incompatible removal of `Volume.parse(String)`

--- docker-java-api/src/main/java/com/github/dockerjava/api/model/Volume.java
+++ docker-java-api/src/main/java/com/github/dockerjava/api/model/Volume.java
@@ -1,6 +1,7 @@
 package com.github.dockerjava.api.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.EqualsAndHashCode;
 
@@ -23,6 +24,18 @@ import java.util.Map;
 @EqualsAndHashCode
 public class Volume implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    /**
+     * Handles the {@code { "Destination" : { "path" : "/path/to/mount" } }} variant.
+     * @param path the destination path of the bind mounted volume
+     * @return a volume instance referring to the given path.
+     * @deprecated use {@link #parse(Map)}
+     */
+    @Nonnull
+    @Deprecated
+    public static Volume parse(@JsonProperty("path") String path) {
+        return new Volume(path);
+    }
 
     /**
      * Handles the {@code { "Destination" : { "path" : "/path/to/mount" } }} variant.
