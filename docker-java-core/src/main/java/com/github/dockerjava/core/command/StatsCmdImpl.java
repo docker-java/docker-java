ffec4f598f1ef46c2192b34470781bd38c6e2e59
Alexey Atiskov <atiskov@gmail.com>
2020-02-14 14:04:43 +0300

    Introduce boolean nostream parameter to StatsCmd. (#1287)
    * BUG-1278: Introduce boolean nostream parameter to StatsCmd.
    
    * BUG-1278: Fix review. User try-with-resources. Remove sleep. Use null-safe check.
    
    * BUG-1278: Fix review. Removed useless assertions and logs. Fix namings. Fix container names. Revert encodings.xml.
    
    * BUG-1278: Remove not used imports.
    
    Fixes #1278

--- docker-java-core/src/main/java/com/github/dockerjava/core/command/StatsCmdImpl.java
+++ docker-java-core/src/main/java/com/github/dockerjava/core/command/StatsCmdImpl.java
@@ -12,6 +12,8 @@ public class StatsCmdImpl extends AbstrAsyncDockerCmd<StatsCmd, Statistics> impl
 
     private String containerId;
 
+    private Boolean noStream;
+
     public StatsCmdImpl(StatsCmd.Exec exec, String containerId) {
         super(exec);
         withContainerId(containerId);
@@ -29,4 +31,14 @@ public class StatsCmdImpl extends AbstrAsyncDockerCmd<StatsCmd, Statistics> impl
         return containerId;
     }
 
+    @Override
+    public Boolean hasNoStream() {
+        return noStream;
+    }
+
+    @Override
+    public StatsCmd withNoStream(boolean noStream) {
+        this.noStream = noStream;
+        return this;
+    }
 }
