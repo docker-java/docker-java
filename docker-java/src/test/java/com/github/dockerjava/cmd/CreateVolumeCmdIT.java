bfdbbbbc930e97f8a45dde90957217e6ea404ae9
Andrii Mazurok <andrew_maz@ukr.net>
2020-02-15 20:35:45 +0200

    Labels for volume has been added (#1320)

--- docker-java/src/test/java/com/github/dockerjava/cmd/CreateVolumeCmdIT.java
+++ docker-java/src/test/java/com/github/dockerjava/cmd/CreateVolumeCmdIT.java
@@ -4,6 +4,8 @@ import com.github.dockerjava.api.command.CreateVolumeResponse;
 import com.github.dockerjava.api.exception.DockerException;
 import org.junit.Test;
 
+import java.util.Collections;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
@@ -16,10 +18,11 @@ public class CreateVolumeCmdIT extends CmdIT {
         String volumeName = "volume1";
 
         CreateVolumeResponse createVolumeResponse = dockerRule.getClient().createVolumeCmd().withName(volumeName)
-                .withDriver("local").exec();
+                .withDriver("local").withLabels(Collections.singletonMap("is-timelord", "yes")).exec();
 
         assertThat(createVolumeResponse.getName(), equalTo(volumeName));
         assertThat(createVolumeResponse.getDriver(), equalTo("local"));
+        assertThat(createVolumeResponse.getLabels(), equalTo(Collections.singletonMap("is-timelord", "yes")));
         assertThat(createVolumeResponse.getMountpoint(), containsString("/volume1/"));
     }
 
@@ -29,17 +32,19 @@ public class CreateVolumeCmdIT extends CmdIT {
         String volumeName = "volume1";
 
         CreateVolumeResponse createVolumeResponse1 = dockerRule.getClient().createVolumeCmd().withName(volumeName)
-                .withDriver("local").exec();
+                .withDriver("local").withLabels(Collections.singletonMap("is-timelord", "yes")).exec();
 
         assertThat(createVolumeResponse1.getName(), equalTo(volumeName));
         assertThat(createVolumeResponse1.getDriver(), equalTo("local"));
+        assertThat(createVolumeResponse1.getLabels(), equalTo(Collections.singletonMap("is-timelord", "yes")));
         assertThat(createVolumeResponse1.getMountpoint(), containsString("/volume1/"));
 
         CreateVolumeResponse createVolumeResponse2 = dockerRule.getClient().createVolumeCmd().withName(volumeName)
-                .withDriver("local").exec();
+                .withDriver("local").withLabels(Collections.singletonMap("is-timelord", "yes")).exec();
 
         assertThat(createVolumeResponse2.getName(), equalTo(volumeName));
         assertThat(createVolumeResponse2.getDriver(), equalTo("local"));
+        assertThat(createVolumeResponse2.getLabels(), equalTo(Collections.singletonMap("is-timelord", "yes")));
         assertThat(createVolumeResponse2.getMountpoint(), equalTo(createVolumeResponse1.getMountpoint()));
     }
 }
