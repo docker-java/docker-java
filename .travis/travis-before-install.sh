7ac85cf9da1e77398eb47d6a66cebe4f12d199da
Sergei Egorov <segorov@pivotal.io>
2020-04-03 19:00:49 +0200

    try

--- .travis/travis-before-install.sh
+++ .travis/travis-before-install.sh
@@ -27,7 +27,7 @@ if [[ -n $DOCKER_HOST ]]; then
     sudo mkdir -p /etc/systemd/system/docker.service.d/
     cat << EOF | sudo tee /etc/systemd/system/docker.service.d/override.conf
 [Service]
-ExecStart="/usr/bin/dockerd -H fd://"
+ExecStart="/usr/bin/dockerd -H unix:///var/run/docker.sock -H tcp://127.0.0.1:${HOST_PORT} --label=com.github.dockerjava.test=docker-java"
 EOF
 
     sudo systemctl daemon-reload
