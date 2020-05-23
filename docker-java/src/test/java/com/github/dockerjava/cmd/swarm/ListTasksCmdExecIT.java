a09fefe832fc87df70e80b74fa0891fc3aed246d
Sergei Egorov <segorov@pivotal.io>
2020-05-23 19:39:01 +0200

    Port Travis to GHA (#1396)
    - removes Travis config
    - adds "Docker 18.x" and "Docker over TCP" to GHA Build Matrix
    - adds ciMate
    - Fixes Swarm tests

--- docker-java/src/test/java/com/github/dockerjava/cmd/swarm/ListTasksCmdExecIT.java
+++ docker-java/src/test/java/com/github/dockerjava/cmd/swarm/ListTasksCmdExecIT.java
@@ -6,10 +6,10 @@ import com.github.dockerjava.api.model.ContainerSpec;
 import com.github.dockerjava.api.model.ServiceModeConfig;
 import com.github.dockerjava.api.model.ServiceReplicatedModeOptions;
 import com.github.dockerjava.api.model.ServiceSpec;
-import com.github.dockerjava.api.model.SwarmSpec;
 import com.github.dockerjava.api.model.Task;
 import com.github.dockerjava.api.model.TaskSpec;
 import com.github.dockerjava.api.model.TaskState;
+import org.junit.After;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,24 +17,27 @@ import org.slf4j.LoggerFactory;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import static com.github.dockerjava.junit.DockerRule.DEFAULT_IMAGE;
+import static org.awaitility.Awaitility.await;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
 public class ListTasksCmdExecIT extends SwarmCmdIT {
     public static final Logger LOG = LoggerFactory.getLogger(CreateServiceCmdExecIT.class);
-    private static final String SERVICE_NAME = "inspect_task";
+    private final String SERVICE_NAME = "inspect_task";
     private static final String TASK_LABEL_KEY = "com.github.dockerjava.usage";
     private static final String TASK_LABEL_VALUE = "test";
 
+    @After
+    public void tearDown() {
+        dockerRule.getClient().removeServiceCmd(SERVICE_NAME).exec();
+    }
+
     @Test
     public void testListTasks() throws DockerException {
-        dockerRule.getClient().initializeSwarmCmd(new SwarmSpec())
-                .withListenAddr("127.0.0.1")
-                .withAdvertiseAddr("127.0.0.1")
-                .exec();
         Map<String, String> taskLabels = Collections.singletonMap(TASK_LABEL_KEY, TASK_LABEL_VALUE);
         CreateServiceResponse response = dockerRule.getClient().createServiceCmd(new ServiceSpec()
                 .withName(SERVICE_NAME)
@@ -50,15 +53,22 @@ public class ListTasksCmdExecIT extends SwarmCmdIT {
         //filtering with service id
         List<Task> tasks = dockerRule.getClient().listTasksCmd().withServiceFilter(serviceId).exec();
         assertThat(tasks, hasSize(2));
-        String taskId = tasks.get(0).getId(), secondId = tasks.get(1).getId();
+        String taskId = tasks.get(0).getId();
+        String secondTaskId = tasks.get(1).getId();
         //filtering with unique id
         tasks = dockerRule.getClient().listTasksCmd().withIdFilter(taskId).exec();
         assertThat(tasks, hasSize(1));
         assertThat(tasks.get(0).getId(), is(taskId));
         //filtering with multiple id
-        tasks = dockerRule.getClient().listTasksCmd().withIdFilter(secondId, taskId).exec();
+        tasks = dockerRule.getClient().listTasksCmd().withIdFilter(secondTaskId, taskId).exec();
+        assertThat(tasks, hasSize(2));
         //filtering node id
-        String nodeId = tasks.get(0).getNodeId();
+        // Wait for node assignment
+        String nodeId = await().until(() -> {
+            return dockerRule.getClient().listTasksCmd().withIdFilter(secondTaskId).exec()
+                .get(0)
+                .getNodeId();
+        }, Objects::nonNull);
         tasks = dockerRule.getClient().listTasksCmd().withNodeFilter(nodeId).exec();
         assertThat(tasks.get(0).getNodeId(), is(nodeId));
         //filtering with state
@@ -69,6 +79,5 @@ public class ListTasksCmdExecIT extends SwarmCmdIT {
         assertThat(tasks, hasSize(2));
         tasks = dockerRule.getClient().listTasksCmd().withLabelFilter(TASK_LABEL_KEY + "=" + TASK_LABEL_VALUE).exec();
         assertThat(tasks, hasSize(2));
-        dockerRule.getClient().removeServiceCmd(SERVICE_NAME).exec();
     }
 }
