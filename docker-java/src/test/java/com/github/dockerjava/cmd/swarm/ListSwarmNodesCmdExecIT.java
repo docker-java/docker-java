531dd699f32765650c8b7825b3688a364375e220
Sergei Egorov <bsideup@gmail.com>
2020-05-23 19:47:50 +0200

    Update docker-java/src/test/java/com/github/dockerjava/cmd/swarm/ListSwarmNodesCmdExecIT.java

--- docker-java/src/test/java/com/github/dockerjava/cmd/swarm/ListSwarmNodesCmdExecIT.java
+++ docker-java/src/test/java/com/github/dockerjava/cmd/swarm/ListSwarmNodesCmdExecIT.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class ListSwarmNodesCmdExecIT extends SwarmCmdIT {
+public class ListSwarmNodesCmdExecIT extends MultiNodeSwarmCmdIT {
     @Test
     public void testListSwarmNodes() throws Exception {
         DockerClient docker = startDockerInDocker();
