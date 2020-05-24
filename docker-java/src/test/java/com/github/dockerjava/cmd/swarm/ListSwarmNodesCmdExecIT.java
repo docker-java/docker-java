f5965ea095802b9a48fa5ba3a50d338d2a95d58d
Sergei Egorov <bsideup@gmail.com>
2020-05-24 20:47:47 +0200

    use `startSwarm()`

--- docker-java/src/test/java/com/github/dockerjava/cmd/swarm/ListSwarmNodesCmdExecIT.java
+++ docker-java/src/test/java/com/github/dockerjava/cmd/swarm/ListSwarmNodesCmdExecIT.java
@@ -2,7 +2,6 @@ package com.github.dockerjava.cmd.swarm;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.model.SwarmNode;
-import com.github.dockerjava.api.model.SwarmSpec;
 import org.junit.Test;
 
 import java.util.Collections;
@@ -14,28 +13,26 @@ import static org.hamcrest.Matchers.is;
 public class ListSwarmNodesCmdExecIT extends SwarmCmdIT {
     @Test
     public void testListSwarmNodes() throws Exception {
-        DockerClient docker = startDockerInDocker();
-        docker.initializeSwarmCmd(new SwarmSpec()).exec();
+        DockerClient dockerClient = startSwarm();
 
-        List<SwarmNode> nodes = docker.listSwarmNodesCmd().exec();
+        List<SwarmNode> nodes = dockerClient.listSwarmNodesCmd().exec();
         assertThat(nodes.size(), is(1));
     }
 
     @Test
     public void testListSwarmNodesWithIdFilter() throws Exception {
-        DockerClient docker = startDockerInDocker();
-        docker.initializeSwarmCmd(new SwarmSpec()).exec();
+        DockerClient dockerClient = startSwarm();
 
-        List<SwarmNode> nodes = docker.listSwarmNodesCmd().exec();
+        List<SwarmNode> nodes = dockerClient.listSwarmNodesCmd().exec();
         assertThat(nodes.size(), is(1));
 
         String nodeId = nodes.get(0).getId();
-        List<SwarmNode> nodesWithId = docker.listSwarmNodesCmd()
+        List<SwarmNode> nodesWithId = dockerClient.listSwarmNodesCmd()
             .withIdFilter(Collections.singletonList(nodeId))
             .exec();
         assertThat(nodesWithId.size(), is(1));
 
-        List<SwarmNode> nodesWithNonexistentId = docker.listSwarmNodesCmd()
+        List<SwarmNode> nodesWithNonexistentId = dockerClient.listSwarmNodesCmd()
             .withIdFilter(Collections.singletonList("__nonexistent__"))
             .exec();
         assertThat(nodesWithNonexistentId.size(), is(0));
@@ -43,19 +40,18 @@ public class ListSwarmNodesCmdExecIT extends SwarmCmdIT {
 
     @Test
     public void testListSwarmNodesWithNameFilter() throws Exception {
-        DockerClient docker = startDockerInDocker();
-        docker.initializeSwarmCmd(new SwarmSpec()).exec();
+        DockerClient dockerClient = startSwarm();
 
-        List<SwarmNode> nodes = docker.listSwarmNodesCmd().exec();
+        List<SwarmNode> nodes = dockerClient.listSwarmNodesCmd().exec();
         assertThat(nodes.size(), is(1));
 
         String nodeName = nodes.get(0).getSpec().getName();
-        List<SwarmNode> nodesWithFirstNodesName = docker.listSwarmNodesCmd()
+        List<SwarmNode> nodesWithFirstNodesName = dockerClient.listSwarmNodesCmd()
             .withNameFilter(Collections.singletonList(nodeName))
             .exec();
         assertThat(nodesWithFirstNodesName.size(), is(1));
 
-        List<SwarmNode> nodesWithNonexistentName = docker.listSwarmNodesCmd()
+        List<SwarmNode> nodesWithNonexistentName = dockerClient.listSwarmNodesCmd()
             .withNameFilter(Collections.singletonList("__nonexistent__"))
             .exec();
         assertThat(nodesWithNonexistentName.size(), is(0));
@@ -63,15 +59,14 @@ public class ListSwarmNodesCmdExecIT extends SwarmCmdIT {
 
     @Test
     public void testListSwarmNodesWithMembershipFilter() throws Exception {
-        DockerClient docker = startDockerInDocker();
-        docker.initializeSwarmCmd(new SwarmSpec()).exec();
+        DockerClient dockerClient = startSwarm();
 
-        List<SwarmNode> nodesWithAcceptedMembership = docker.listSwarmNodesCmd()
+        List<SwarmNode> nodesWithAcceptedMembership = dockerClient.listSwarmNodesCmd()
             .withMembershipFilter(Collections.singletonList("accepted"))
             .exec();
         assertThat(nodesWithAcceptedMembership.size(), is(1));
 
-        List<SwarmNode> nodesWithPendingMembership = docker.listSwarmNodesCmd()
+        List<SwarmNode> nodesWithPendingMembership = dockerClient.listSwarmNodesCmd()
             .withMembershipFilter(Collections.singletonList("pending"))
             .exec();
         assertThat(nodesWithPendingMembership.size(), is(0));
@@ -79,15 +74,14 @@ public class ListSwarmNodesCmdExecIT extends SwarmCmdIT {
 
     @Test
     public void testListSwarmNodesWithRoleFilter() throws Exception {
-        DockerClient docker = startDockerInDocker();
-        docker.initializeSwarmCmd(new SwarmSpec()).exec();
+        DockerClient dockerClient = startSwarm();
 
-        List<SwarmNode> nodesWithManagerRole = docker.listSwarmNodesCmd()
+        List<SwarmNode> nodesWithManagerRole = dockerClient.listSwarmNodesCmd()
             .withRoleFilter(Collections.singletonList("manager"))
             .exec();
         assertThat(nodesWithManagerRole.size(), is(1));
 
-        List<SwarmNode> nodesWithWorkerRole = docker.listSwarmNodesCmd()
+        List<SwarmNode> nodesWithWorkerRole = dockerClient.listSwarmNodesCmd()
             .withRoleFilter(Collections.singletonList("worker"))
             .exec();
         assertThat(nodesWithWorkerRole.size(), is(0));
