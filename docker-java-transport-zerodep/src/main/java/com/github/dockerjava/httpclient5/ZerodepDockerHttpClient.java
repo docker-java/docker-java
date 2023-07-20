61192203a9d28953a196662ca04ebfeb33393a29
Robert Young <robeyoun@redhat.com>
2023-07-20 12:39:29 +1200

    Expose httpclient5 connection keepalive
    Why:
    We have been experiencing problems with podman and testcontainers using a
    globally cached ZerodepDockerHttpClient. The problem is, with it's default configuration,
    podman closes idle connections after 10 seconds. The apache HTTP client's default
    connection keepalive is 3 minutes. So we are seeing exceptions thrown when the client
    attempts to use these stale connections that it has in the connection pool.
    
    A configuration that could help are reducing the connectionKeepAlive, so we could
    configure the pool to close connections in line with this podman timeout.
    
    Note: stale connection checking does not work and blocks in the isStale see #1726
    
    https://github.com/testcontainers/testcontainers-java/issues/7310

--- docker-java-transport-zerodep/src/main/java/com/github/dockerjava/httpclient5/ZerodepDockerHttpClient.java
+++ docker-java-transport-zerodep/src/main/java/com/github/dockerjava/httpclient5/ZerodepDockerHttpClient.java
@@ -20,6 +20,8 @@ public final class ZerodepDockerHttpClient extends ApacheDockerHttpClientImpl {
 
         private Duration responseTimeout;
 
+        private Duration connectionKeepAlive;
+
         public Builder dockerHost(URI value) {
             this.dockerHost = Objects.requireNonNull(value, "dockerHost");
             return this;
@@ -45,14 +47,20 @@ public final class ZerodepDockerHttpClient extends ApacheDockerHttpClientImpl {
             return this;
         }
 
+        public Builder connectionKeepAlive(Duration connectionKeepAlive) {
+            this.connectionKeepAlive = connectionKeepAlive;
+            return this;
+        }
+
         public ZerodepDockerHttpClient build() {
             Objects.requireNonNull(dockerHost, "dockerHost");
-            return new ZerodepDockerHttpClient(dockerHost, sslConfig, maxConnections, connectionTimeout, responseTimeout);
+            return new ZerodepDockerHttpClient(dockerHost, sslConfig, maxConnections, connectionTimeout, responseTimeout,
+                connectionKeepAlive);
         }
     }
 
     private ZerodepDockerHttpClient(URI dockerHost, SSLConfig sslConfig, int maxConnections, Duration connectionTimeout,
-        Duration responseTimeout) {
-        super(dockerHost, sslConfig, maxConnections, connectionTimeout, responseTimeout);
+        Duration responseTimeout, Duration connectionKeepAlive) {
+        super(dockerHost, sslConfig, maxConnections, connectionTimeout, responseTimeout, connectionKeepAlive);
     }
 }
