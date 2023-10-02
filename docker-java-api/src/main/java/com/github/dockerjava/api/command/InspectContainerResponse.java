98d17d22a6ddd5058f06b055810deabd5a768418
l.s.andringa1 <l.s.andringa@rug.nl>
2023-10-02 23:55:52 +0200

    Implemented the `SizeRw` field in the `InspectContainerResponse` class.

--- docker-java-api/src/main/java/com/github/dockerjava/api/command/InspectContainerResponse.java
+++ docker-java-api/src/main/java/com/github/dockerjava/api/command/InspectContainerResponse.java
@@ -63,6 +63,9 @@ public class InspectContainerResponse extends DockerObject {
     @JsonProperty("SizeRootFs")
     private Integer sizeRootFs;
 
+    @JsonPropertY("SizeRw")
+    private Integer sizeRw;
+
     @JsonProperty("Image")
     private String imageId;
 
@@ -123,6 +126,10 @@ public class InspectContainerResponse extends DockerObject {
 
     public Integer getSizeRootFs() {
         return sizeRootFs;
+    }
+
+    public Inte ger getSizeRw() {
+        return sizeRw;
     }
 
     public String getCreated() {
