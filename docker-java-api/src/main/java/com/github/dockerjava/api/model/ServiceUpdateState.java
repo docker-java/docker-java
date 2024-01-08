bdf5bf028ba39b98dea3e59761dc4c6174779742
Sylvere Richard <sylvere.richard@gmail.com>
2024-01-08 23:12:00 +0100

    Add missing states in ServiceUpdateState

--- docker-java-api/src/main/java/com/github/dockerjava/api/model/ServiceUpdateState.java
+++ docker-java-api/src/main/java/com/github/dockerjava/api/model/ServiceUpdateState.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @since {@link RemoteApiVersion#VERSION_1_24}
  */
 public enum ServiceUpdateState {
+    @JsonProperty("unknown")
+    UNKNOWN,
+
     @JsonProperty("updating")
     UPDATING,
 
@@ -14,6 +17,12 @@ public enum ServiceUpdateState {
 
     @JsonProperty("completed")
     COMPLETED,
+
+    @JsonProperty("rollback_started")
+    ROLLBACK_STARTED,
+
+    @JsonProperty("rollback_paused")
+    ROLLBACK_PAUSED,
 
     @JsonProperty("rollback_completed")
     ROLLBACK_COMPLETED
