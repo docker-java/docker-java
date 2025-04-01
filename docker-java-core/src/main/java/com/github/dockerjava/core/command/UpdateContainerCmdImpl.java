1d6c3445c7877e0511e1fcb55708568b254831f6
Nikita Nikolenko <nikit011418@gmail.com>
2025-04-01 20:46:49 +0200

    Add some missed options to UpdateContainerCmd (#2389)
    * Add some missed options to UpdateContainerCmd
    
    * Delete old cpuPeriod and cpuQuota functions
    
    * Add update container response tests

--- docker-java-core/src/main/java/com/github/dockerjava/core/command/UpdateContainerCmdImpl.java
+++ docker-java-core/src/main/java/com/github/dockerjava/core/command/UpdateContainerCmdImpl.java
@@ -4,6 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.dockerjava.api.command.UpdateContainerCmd;
 import com.github.dockerjava.api.exception.NotFoundException;
+import com.github.dockerjava.api.model.BlkioRateDevice;
+import com.github.dockerjava.api.model.BlkioWeightDevice;
+import com.github.dockerjava.api.model.Device;
+import com.github.dockerjava.api.model.DeviceRequest;
+import com.github.dockerjava.api.model.RestartPolicy;
+import com.github.dockerjava.api.model.Ulimit;
 import com.github.dockerjava.api.model.UpdateContainerResponse;
 import com.github.dockerjava.core.RemoteApiVersion;
 import org.apache.commons.lang3.builder.EqualsBuilder;
@@ -12,6 +18,7 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
+import java.util.List;
 
 /**
  * @author Kanstantsin Shautsou
@@ -28,20 +35,56 @@ public class UpdateContainerCmdImpl extends AbstrDockerCmd<UpdateContainerCmd, U
     @JsonProperty("BlkioWeight")
     private Integer blkioWeight;
 
+    @JsonProperty("BlkioWeightDevice")
+    private List<BlkioWeightDevice> blkioWeightDevice;
+
+    @JsonProperty("BlkioDeviceReadBps")
+    private List<BlkioRateDevice> blkioDeviceReadBps;
+
+    @JsonProperty("BlkioDeviceWriteBps")
+    private List<BlkioRateDevice> blkioDeviceWriteBps;
+
+    @JsonProperty("BlkioDeviceReadIOps")
+    private List<BlkioRateDevice> blkioDeviceReadIOps;
+
+    @JsonProperty("BlkioDeviceWriteIOps")
+    private List<BlkioRateDevice> blkioDeviceWriteIOps;
+
     @JsonProperty("CpuShares")
     private Integer cpuShares;
 
     @JsonProperty("CpuPeriod")
-    private Integer cpuPeriod;
+    private Long cpuPeriod;
 
     @JsonProperty("CpuQuota")
-    private Integer cpuQuota;
+    private Long cpuQuota;
+
+    @JsonProperty("CpuRealtimePeriod")
+    private Long cpuRealtimePeriod;
+
+    @JsonProperty("CpuRealtimeRuntime")
+    private Long cpuRealtimeRuntime;
+
+    @JsonProperty("NanoCpus")
+    private Long nanoCPUs;
 
     @JsonProperty("CpusetCpus")
     private String cpusetCpus;
 
     @JsonProperty("CpusetMems")
     private String cpusetMems;
+
+    @JsonProperty("Devices")
+    private List<Device> devices;
+
+    @JsonProperty("DeviceCgroupRules")
+    private List<String> deviceCgroupRules;
+
+    /**
+     * @since {@link com.github.dockerjava.core.RemoteApiVersion#VERSION_1_40}
+     */
+    @JsonProperty("DeviceRequests")
+    private List<DeviceRequest> deviceRequests;
 
     @JsonProperty("Memory")
     private Long memory;
@@ -55,25 +98,24 @@ public class UpdateContainerCmdImpl extends AbstrDockerCmd<UpdateContainerCmd, U
     @JsonProperty("KernelMemory")
     private Long kernelMemory;
 
+    @JsonProperty("OomKillDisable")
+    private Boolean oomKillDisable;
+
+    @JsonProperty("Init")
+    private Boolean init;
+
+    @JsonProperty("PidsLimit")
+    private Long pidsLimit;
+
+    @JsonProperty("Ulimits")
+    private List<Ulimit> ulimits;
+
+    @JsonProperty("RestartPolicy")
+    private RestartPolicy restartPolicy;
+
     public UpdateContainerCmdImpl(UpdateContainerCmd.Exec exec, String containerId) {
         super(exec);
         withContainerId(containerId);
-    }
-
-    /**
-     * @see #blkioWeight
-     */
-    @CheckForNull
-    public Integer getBlkioWeight() {
-        return blkioWeight;
-    }
-
-    /**
-     * @see #blkioWeight
-     */
-    public UpdateContainerCmd withBlkioWeight(Integer blkioWeight) {
-        this.blkioWeight = blkioWeight;
-        return this;
     }
 
     /**
@@ -94,17 +136,99 @@ public class UpdateContainerCmdImpl extends AbstrDockerCmd<UpdateContainerCmd, U
     }
 
     /**
+     * @see #blkioWeight
+     */
+    @CheckForNull
+    public Integer getBlkioWeight() {
+        return blkioWeight;
+    }
+
+    /**
+     * @see #blkioWeight
+     */
+    public UpdateContainerCmd withBlkioWeight(Integer blkioWeight) {
+        this.blkioWeight = blkioWeight;
+        return this;
+    }
+
+    /**
+     * @see #blkioWeightDevice
+     */
+    @CheckForNull
+    public List<BlkioWeightDevice> getBlkioWeightDevice() {
+        return blkioWeightDevice;
+    }
+
+    public UpdateContainerCmd withBlkioWeightDevice(List<BlkioWeightDevice> blkioWeightDevice) {
+        this.blkioWeightDevice = blkioWeightDevice;
+        return this;
+    }
+
+    /**
+     * @see #blkioDeviceReadBps
+     */
+    @CheckForNull
+    public List<BlkioRateDevice> getBlkioDeviceReadBps() {
+        return blkioDeviceReadBps;
+    }
+
+    public UpdateContainerCmd withBlkioDeviceReadBps(List<BlkioRateDevice> blkioDeviceReadBps) {
+        this.blkioDeviceReadBps = blkioDeviceReadBps;
+        return this;
+    }
+
+    /**
+     * @see #blkioDeviceWriteBps
+     */
+    @CheckForNull
+    public List<BlkioRateDevice> getBlkioDeviceWriteBps() {
+        return blkioDeviceWriteBps;
+    }
+
+    public UpdateContainerCmd withBlkioDeviceWriteBps(List<BlkioRateDevice> blkioDeviceWriteBps) {
+        this.blkioDeviceWriteBps = blkioDeviceWriteBps;
+        return this;
+    }
+
+    /**
+     * @see #blkioDeviceReadIOps
+     */
+    @CheckForNull
+    public List<BlkioRateDevice> getBlkioDeviceReadIOps() {
+        return blkioDeviceReadIOps;
+    }
+
+    public UpdateContainerCmd withBlkioDeviceReadIOps(List<BlkioRateDevice> blkioDeviceReadIOps) {
+        this.blkioDeviceReadIOps = blkioDeviceReadIOps;
+        return this;
+    }
+
+    /**
+     * @see #blkioDeviceWriteIOps
+     */
+    @CheckForNull
+    public List<BlkioRateDevice> getBlkioDeviceWriteIOps() {
+        return blkioDeviceWriteIOps;
+    }
+
+    @Override
+    public UpdateContainerCmd withBlkioDeviceWriteIOps(List<BlkioRateDevice> blkioDeviceWriteIOps) {
+        this.blkioDeviceWriteIOps = blkioDeviceWriteIOps;
+        return this;
+    }
+
+    /**
      * @see #cpuPeriod
      */
     @CheckForNull
-    public Integer getCpuPeriod() {
+    public Long getCpuPeriod() {
         return cpuPeriod;
     }
 
     /**
      * @see #cpuPeriod
      */
-    public UpdateContainerCmd withCpuPeriod(Integer cpuPeriod) {
+    public UpdateContainerCmd withCpuPeriod(Long cpuPeriod) {
         this.cpuPeriod = cpuPeriod;
         return this;
     }
@@ -113,14 +237,14 @@ public class UpdateContainerCmdImpl extends AbstrDockerCmd<UpdateContainerCmd, U
      * @see #cpuQuota
      */
     @CheckForNull
-    public Integer getCpuQuota() {
+    public Long getCpuQuota() {
         return cpuQuota;
     }
 
     /**
      * @see #cpuQuota
      */
-    public UpdateContainerCmd withCpuQuota(Integer cpuQuota) {
+    public UpdateContainerCmd withCpuQuota(Long cpuQuota) {
         this.cpuQuota = cpuQuota;
         return this;
     }
@@ -170,6 +294,71 @@ public class UpdateContainerCmdImpl extends AbstrDockerCmd<UpdateContainerCmd, U
      */
     public UpdateContainerCmd withCpuShares(Integer cpuShares) {
         this.cpuShares = cpuShares;
+        return this;
+    }
+
+    /**
+     * @see #cpuRealtimePeriod
+     */
+    @CheckForNull
+    public Long getCpuRealtimePeriod() {
+        return cpuRealtimePeriod;
+    }
+
+    public UpdateContainerCmd withCpuRealtimePeriod(Long cpuRealtimePeriod) {
+        this.cpuRealtimePeriod = cpuRealtimePeriod;
+        return this;
+    }
+
+    /**
+     * @see #cpuRealtimeRuntime
+     */
+    @CheckForNull
+    public Long getCpuRealtimeRuntime() {
+        return cpuRealtimeRuntime;
+    }
+
+    public UpdateContainerCmd withCpuRealtimeRuntime(Long cpuRealtimeRuntime) {
+        this.cpuRealtimeRuntime = cpuRealtimeRuntime;
+        return this;
+    }
+
+    /**
+     * @see #devices
+     */
+    @CheckForNull
+    public List<Device> getDevices() {
+        return devices;
+    }
+
+    public UpdateContainerCmd withDevices(List<Device> devices) {
+        this.devices = devices;
+        return this;
+    }
+
+    /**
+     * @see #deviceCgroupRules
+     */
+    @CheckForNull
+    public List<String> getDeviceCgroupRules() {
+        return deviceCgroupRules;
+    }
+
+    public UpdateContainerCmd withDeviceCgroupRules(List<String> deviceCgroupRules) {
+        this.deviceCgroupRules = deviceCgroupRules;
+        return this;
+    }
+
+    /**
+     * @see #deviceRequests
+     */
+    @CheckForNull
+    public List<DeviceRequest> getDeviceRequests() {
+        return deviceRequests;
+    }
+
+    public UpdateContainerCmd withDeviceRequests(List<DeviceRequest> deviceRequests) {
+        this.deviceRequests = deviceRequests;
         return this;
     }
 
@@ -234,6 +423,84 @@ public class UpdateContainerCmdImpl extends AbstrDockerCmd<UpdateContainerCmd, U
      */
     public UpdateContainerCmd withMemorySwap(Long memorySwap) {
         this.memorySwap = memorySwap;
+        return this;
+    }
+
+    /**
+     * @see #nanoCPUs
+     */
+    @CheckForNull
+    public Long getNanoCPUs() {
+        return nanoCPUs;
+    }
+
+    public UpdateContainerCmd withNanoCPUs(Long nanoCPUs) {
+        this.nanoCPUs = nanoCPUs;
+        return this;
+    }
+
+    /**
+     * @see #oomKillDisable
+     */
+    @CheckForNull
+    public Boolean getOomKillDisable() {
+        return oomKillDisable;
+    }
+
+    public UpdateContainerCmd withOomKillDisable(Boolean oomKillDisable) {
+        this.oomKillDisable = oomKillDisable;
+        return this;
+    }
+
+    /**
+     * @see #init
+     */
+    @CheckForNull
+    public Boolean getInit() {
+        return init;
+    }
+
+    public UpdateContainerCmd withInit(Boolean init) {
+        this.init = init;
+        return this;
+    }
+
+    /**
+     * @see #pidsLimit
+     */
+    @CheckForNull
+    public Long getPidsLimit() {
+        return pidsLimit;
+    }
+
+    public UpdateContainerCmd withPidsLimit(Long pidsLimit) {
+        this.pidsLimit = pidsLimit;
+        return this;
+    }
+
+    /**
+     * @see #ulimits
+     */
+    @CheckForNull
+    public List<Ulimit> getUlimits() {
+        return ulimits;
+    }
+
+    public UpdateContainerCmd withUlimits(List<Ulimit> ulimits) {
+        this.ulimits = ulimits;
+        return this;
+    }
+
+    /**
+     * @see #restartPolicy
+     */
+    @CheckForNull
+    public RestartPolicy getRestartPolicy() {
+        return restartPolicy;
+    }
+
+    public UpdateContainerCmd withRestartPolicy(RestartPolicy restartPolicy) {
+        this.restartPolicy = restartPolicy;
         return this;
     }
 
